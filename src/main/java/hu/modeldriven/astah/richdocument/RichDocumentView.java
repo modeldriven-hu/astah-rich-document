174ef7f7908164567f2d196764db9e615636b451
Zsolt Sandor <zsolt.sandor@gmail.com>
2024-01-21 14:14:56 +0100

    Added initial code

---
+++ src/main/java/hu/modeldriven/astah/richdocument/RichDocumentView.java
@@ -0,0 +1,63 @@
+package hu.modeldriven.astah.richdocument;
+
+import com.change_vision.jude.api.inf.AstahAPI;
+import com.change_vision.jude.api.inf.ui.IPluginExtraTabView;
+import com.change_vision.jude.api.inf.ui.ISelectionListener;
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+
+public class RichDocumentView extends JPanel implements IPluginExtraTabView {
+
+    public RichDocumentView() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setLayout(new BorderLayout());
+        add(createContentPane(), BorderLayout.CENTER);
+    }
+
+    private Container createContentPane() {
+
+        Component component = null;
+
+        try {
+            component = AstahAPI.getAstahAPI().getViewManager().getMainFrame();
+        } catch (Exception e) {
+            component = this;
+        }
+
+        return null;
+    }
+
+    @Override
+    public void addSelectionListener(ISelectionListener listener) {
+        // nothing to do here
+    }
+
+    @Override
+    public Component getComponent() {
+        return this;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Rich Document View";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Rich Document";
+    }
+
+    public void activated() {
+        // nothing to do here
+    }
+
+    public void deactivated() {
+        // nothing to do here
+    }
+}
